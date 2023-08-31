7e69323458dc018c487e854a71de870a96b47b53
EnesArslan8 <enes8arslan@gmail.com>
2023-08-31 13:59:08 +0300

    ColorPicker is done

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { SketchPicker } from "react-color";
+import { useSnapshot } from "valtio";
+import store from "../store";
 
 const ColorPicker = () => {
+  const snap = useSnapshot(store);
   return (
-    <div>ColorPicker</div>
-  )
-}
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        onChange={(color) => (store.color = color.hex)}
+        presetColors={[
+            'purple','blue','green','yellow','orange','red'
+        ]}
+      />
+    </div>
+  );
+};
 
-export default ColorPicker
+export default ColorPicker;
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -17,7 +17,31 @@ import {
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+  
 
+  const [file,setFile]=useState('');
+  const [aipromp,setAIpromt]=useState('');
+  const [generatingImg,setGeneratingImg]=useState(false);
+
+  const [activeEditorTab,setActiveEditorTab]=useState("");
+  const [activeFilterTab,setActiveFilterTab]=useState({
+    logoShirt:true,
+    stylishShirt:false,
+  });
+
+  //show tab content depending on the activeTab
+  const generateTabContent=()=>{
+    switch(activeEditorTab){
+      case "colorpicker":
+        return <ColorPicker/>
+      case "filepicker":
+        return <FilePicker/>
+      case "aipicker":
+        return <AIPicker/>
+      default:
+        return null
+    }
+  }
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -32,8 +56,9 @@ const Customizer = () => {
                 className="glassmorphism w-16 border-[2px] rounded-lg flex flex-col justify-center items-center ml-1 py-4 gap-4 tabs"
               >
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                  <Tab key={tab.name} tab={tab} handleClick={() => {setActiveEditorTab(tab.name)}} />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
