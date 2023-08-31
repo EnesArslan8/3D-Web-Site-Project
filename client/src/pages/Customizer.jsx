4a1e288fbdb2b8e5363409b282db0311bbe2e5d3
EnesArslan8 <enes8arslan@gmail.com>
2023-08-31 11:15:31 +0300

    Created Backdrop and CameraRig compenents in Canvas folder

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,17 +1,78 @@
-import React,{useState,useEffect} from 'react'
-import { AnimatePresence } from 'framer-motion'
-import { snapshot } from 'valtio'
-import state from '../store'
-import config from '../config/config'
-import {download} from '../assets'
-import {downloadCanvasToImage,reader} from '../config/helpers'
-import {EditorTabs,FilterTabs,DecalTypes} from '../config/constants'
-import { fadeAnimation,slideAnimation } from '../config/motion'
+import React, { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { snapshot, useSnapshot } from "valtio";
+import state from "../store";
+import config from "../config/config";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+import {
+  AIPicker,
+  ColorPicker,
+  FilePicker,
+  Tab,
+  CustomButton,
+} from "../components";
 
 const Customizer = () => {
-  return (
-    <div>Customizer</div>
-  )
-}
+  const snap = useSnapshot(state);
 
-export default Customizer
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
+            <div className="flex items-center min-h-screen">
+              <div
+                className="glassmorphism w-16 border-[2px] rounded-lg flex flex-col justify-center items-center ml-1 py-4 gap-4 tabs"
+              >
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomButton
+              type={"filled"}
+              title={"Go Back"}
+              handleClick={() => (state.intro = true)}
+              customStyle={"w-fit px-4 py-2.5 font-bold text-sm"}
+            />
+          </motion.div>
+          <motion.div
+            // style={{
+            //   background: "rgba(255, 255, 255, 0.25);",
+            //   boxShadow: " 0 2px 30px 0 rgba(31, 38, 135, 0.007);",
+            //   backdropFilter: "blur(4px);",
+            //   "-webkit-backdrop-filter": "blur(4px);",
+            //   border: "1px solid rgba(255, 255, 255, 0.18);",
+            // }}
+            className="absolute z-10 bottom-5 right-0 left-0 w-full flex justify-center items-center flex-wrap gap-4"
+            {...slideAnimation("up")}
+          >
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab=""
+                handleClick={() => {}}
+              />
+            ))}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Customizer;
