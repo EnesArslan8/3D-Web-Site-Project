5c484201e1ec26597d7e2da4b2d0f70224589d82
EnesArslan8 <enes8arslan@gmail.com>
2023-08-31 11:38:08 +0300

    Camera is done

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { useFrame } from "@react-three/fiber";
+import { Decal, useGLTF, useTexture } from "@react-three/drei";
+
+import store from "../store";
 
 const Shirt = () => {
-  return (
-    <div>Shirt</div>
-  )
-}
+  const snap = useSnapshot(store);
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
 
-export default Shirt
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+  return (
+    <group>
+      <mesh castShadow geometry={nodes.T_Shirt_male.geometry} material={materials.lambert1} material-roughness={1} dispose={null}></mesh>
+    </group>
+  );
+};
+
+export default Shirt;
